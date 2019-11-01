7688a35ebd0d77f03f25148a93a6ba596683cc67
Dave Naugler <davenaugler@gmail.com>
2019-11-01 16:26:55 -0600

    Playing around with Razor in our .cshtml files and using if statments to color a word that starts with C or c

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorFun
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
+
